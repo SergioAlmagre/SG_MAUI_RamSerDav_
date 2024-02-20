b3f978cdde43e2338370c1e21ad5a4166ec567bb
DavidAM <darvendas@gmai.com>
2024-02-20 20:12:48 +0100

    gitflow-feature-stash: #2_Crear_Modelos_Referencia

---
+++ SG_MAUI_RamSerDav_/MVVM/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+
+namespace SQLitePrueba.MVVM.Models
+{
+    [SQLite.Table("Usuarios")]
+    public class Usuario : TableData
+    {
+        [SQLite.Column("Login")]
+        public string Login { get; set; }
+
+        [SQLite.Column("Password")]
+        public string Password { get; set; }
+    }
+}
