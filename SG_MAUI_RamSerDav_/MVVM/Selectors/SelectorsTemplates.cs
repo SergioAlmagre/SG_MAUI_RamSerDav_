521519f36125ef822d463d5e968952a1757f945d
SergioAlmagre <127857356+SergioAlmagre@users.noreply.github.com>
2024-02-20 19:38:55 +0100

    Completada estructura del proyecto

---
+++ SG_MAUI_RamSerDav_/MVVM/Selectors/SelectorsTemplates.cs
@@ -0,0 +1,46 @@
+﻿using GestionProfesores.MVVM.Models;
+using Microsoft.Maui.Controls;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace GestionProfesores.MVVM.Selectors
+{
+    [AddINotifyPropertyChangedInterface]
+    public class SelectorsTemplates : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var profe = (Profesor)item;
+            if (profe.EsJefe)
+            {
+                Application
+                    .Current
+                    .Resources
+                    .TryGetValue("vistaJefe", out var estiloJefe);
+                return (DataTemplate)estiloJefe;
+            }
+            else if (profe.Antiguedad > 10)
+            {
+                Application
+                   .Current
+                   .Resources
+                   .TryGetValue("vistaMasExperiencia", out var estiloExperiencia);
+                return (DataTemplate)estiloExperiencia;
+            }
+            else{
+                Application
+                   .Current
+                   .Resources
+                   .TryGetValue("vistaNormal", out var estiloNormal);
+                return (DataTemplate)estiloNormal;
+            }
+
+        }
+    }
+}
---
+++ SG_MAUI_RamSerDav_/Repositories/BaseRepository.cs
@@ -0,0 +1,169 @@
+﻿
+using SQLite;
+using SQLiteNetExtensions.Extensions;
+using sqlLitePrueba.MVVM.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqlLitePrueba.Repositories
+{
+    public class BaseRepository<T> :
+    IBaseRepository<T> where T : TableData, new()
+    {
+        SQLiteConnection connection;
+        public string StatusMessage { get; set; }
+        public BaseRepository()
+        {
+            connection =
+            new SQLiteConnection(Constantes.DatabasePath,
+            Constantes.Flags);
+            connection.CreateTable<T>();
+        }
+        
+        public void Dispose()
+        {
+            connection.Close();
+        }
+        public T GetItem(int id)
+        {
+            try
+            {
+                return
+                connection.Table<T>()
+                .FirstOrDefault(x => x.Id == id);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage =
+                $"Error: {ex.Message}";
+            }
+            return null;
+        }
+        public T GetItem(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return connection.Table<T>()
+                .Where(predicate).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage =
+                $"Error: {ex.Message}";
+            }
+            return null;
+        }
+        public List<T> GetItems()
+        {
+            try
+            {
+                return connection.Table<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage =
+                $"Error: {ex.Message}";
+            }
+            return null;
+        }
+        public List<T> GetItems(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return connection.Table<T>().Where(predicate).ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage =
+                $"Error: {ex.Message}";
+            }
+            return null;
+        }
+        public List<T> GetItemsCascade()
+                {
+                    try
+                    {
+                        return connection.GetAllWithChildren<T>().ToList();
+                    }
+                    catch (Exception ex)
+                    {
+                        StatusMessage =
+                        $"Error: {ex.Message}";
+                    }
+                    return null;
+                }
+        public void SaveItem(T item)
+        {
+            int result = 0;
+            try
+            {
+                if (item.Id != 0)
+                {
+                    result =
+                    connection.Update(item);
+                    StatusMessage =
+                    $"{result} row(s) updated";
+                }
+                else
+                {
+                    result = connection.Insert(item);
+                    StatusMessage =
+                    $"{result} row(s) added";
+                }
+            }
+            catch (Exception ex)
+            {
+                StatusMessage =
+                $"Error: {ex.Message}";
+            }
+        }
+        public void SaveItemCascade(T item, bool recursive = false)
+        {
+            try
+            {
+                if(item.Id != 0)
+                {
+                    connection.UpdateWithChildren(item);
+                }
+                else
+                {
+                    connection.InsertWithChildren(item);
+                }
+            }catch(Exception ex)
+            {
+                StatusMessage =
+                $"Error: {ex.Message}";
+            }
+        }
+        public void DeleteItem(T item)
+                {
+                    try
+                    {
+                        //connection.Delete(item);
+                        connection.Delete(item);
+                    }
+                    catch (Exception ex)
+                    {
+                        StatusMessage =
+                        $"Error: {ex.Message}";
+                    }
+                }
+        public void DeleteItemCascade(T item)
+        {
+            try
+            {
+                //connection.Delete(item);
+                connection.Delete(item, true);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage =
+                $"Error: {ex.Message}";
+            }
+        }
+    }
+}
