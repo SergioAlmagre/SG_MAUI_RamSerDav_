650ba6cbb7ddd632d3be4cb9edea6d1e11e7bc3f
SergioAlmagre <127857356+SergioAlmagre@users.noreply.github.com>
2024-02-20 17:38:23 +0100

    Añadida carpeta Abstraction

---
+++ SG_MAUI_RamSerDav_/Abstractions/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqlLitePrueba.MVVM.Abstractions
+{
+    public interface IBaseRepository<T> : IDisposable
+    where T : TableData, new()
+    {
+        void SaveItem(T item);
+        void SaveItemCascade(T item, bool recursive=false);
+        T GetItem(int id);
+        T GetItem(Expression<Func<T, bool>> predicate);
+        List<T> GetItems();
+        List<T> GetItemsCascade();
+        List<T> GetItems(Expression<Func<T, bool>> predicate);
+        void DeleteItem(T item);
+        void DeleteItemCascade(T item);
+    }
+}
