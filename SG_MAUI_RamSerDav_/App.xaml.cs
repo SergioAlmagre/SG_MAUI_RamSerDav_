de23d130442d8cce181655d7358fa5f3996ad2ea
SergioAlmagre <127857356+SergioAlmagre@users.noreply.github.com>
2024-02-20 17:02:03 +0100

    Agregar archivos de proyecto.

---
+++ SG_MAUI_RamSerDav_/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SG_MAUI_RamSerDav_
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SG_MAUI_RamSerDav_/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SG_MAUI_RamSerDav_
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
